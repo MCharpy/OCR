425c22eb5563cbf2ee4e3408b286d725bdafe24c
MCharpy <mahe.charpy@epita.fr>
2018-10-06 17:21:30 +0200

    added up to 28*28 Matrix
    Signed-off-by: MCharpy <mahe.charpy@epita.fr>

--- Code/sourceCode/Matrix.c
+++ Code/sourceCode/Matrix.c
@@ -7,20 +7,10 @@ struct Matrix16 {
     float values[16];
 };
 
-typedef struct Matrix784 Matrix784;
-struct Matrix784 {
-    int x;
-    int y;
-    float values[784];
-};
-
 int GetCoordinates16(int x, int y, Matrix16 matrix){
     if(x < 0 || y<0 || x>= matrix.x || y >= matrix.y)
         return -1;
     return y*matrix.y + x;
-
-        
-    
 }
 
 Matrix16 MatrixMult16(Matrix16 matrix1, Matrix16 matrix2){
@@ -45,10 +35,6 @@ Matrix16 MatrixMult16(Matrix16 matrix1, Matrix16 matrix2){
             toReturn.values[m+toReturn.y*p]=sum;
         }
     }
-
-
-
-
     return toReturn;
 }
 
@@ -59,6 +45,53 @@ void printMatrix16(Matrix16 matrix){
             printf("\n");
 
     }
-
-
 }
+
+typedef struct Matrix Matrix;
+struct Matrix {
+    int x;
+    int y;
+    float values[784];
+};
+
+
+int GetCoordinates(int x, int y, Matrix matrix){
+    if(x < 0 || y<0 || x>= matrix.x || y >= matrix.y)
+        return -1;
+    return y*matrix.y + x;
+}
+
+Matrix MatrixMult(Matrix matrix1, Matrix matrix2){
+   Matrix toReturn;
+   if (matrix1.y != matrix2.x){
+        toReturn.x =-1;
+        toReturn.y =-1;
+        return toReturn;
+    }
+    toReturn.x = matrix1.x;
+    toReturn.y = matrix2.y;
+    
+    for (int m = 0 ; m < matrix1.x ; m++)
+    {
+        for (int p = 0 ; p < matrix2.y ; p++)
+        {
+            int sum = 0;
+            for (int k= 0 ; k< matrix1.y ; k++)
+            {
+                sum += matrix1.values[GetCoordinates(m,k,matrix1)]*matrix2.values[GetCoordinates(k,p,matrix2)];
+            }
+            toReturn.values[m+toReturn.y*p]=sum;
+        }
+    }
+    return toReturn;
+}
+
+void printMatrix(Matrix matrix){
+    for (int i = 0; i< matrix.x*matrix.y; i++){
+        printf("%f ",matrix.values[i]);
+        if((i-1)%matrix.x == 0)
+            printf("\n");
+
+    }
+}
+
--- Code/sourceCode/Matrix.h
+++ Code/sourceCode/Matrix.h
@@ -14,5 +14,17 @@ void printMatrix16(Matrix16 matrix);
 Matrix16 MatrixMult16(Matrix16 matrix1, Matrix16 matrix2);
 int GetCoordinates16(int x, int y, Matrix16 matrix);
 
+typedef struct Matrix Matrix;
+struct Matrix{
+  int x;
+  int y;
+  float values[784];
+    
+};
+
+void printMatrix(Matrix matrix);
+Matrix MatrixMult(Matrix matrix1, Matrix matrix2);
+int GetCoordinates(int x, int y, Matrix matrix);
+
 
 #endif
