135cbc1d7318f86cada798b0dd6c65070dac02a9
MCharpy <mahe.charpy@epita.fr>
2018-10-17 16:13:25 +0200

    Started backpropagation implementation
    Signed-off-by: MCharpy <mahe.charpy@epita.fr>

--- Code/sourceCode/OCR.c
+++ Code/sourceCode/OCR.c
@@ -6,25 +6,35 @@
 int main(int argc, char** argv){
 
     //printf("%i",eval(1,0,0));
-    train(10);
+    train(100);
+
+   
     /*
-    Matrix16 mat;
-    mat.x = 2;
-    mat.y = 2;
+    Matrix16 mat1;
+    Matrix16 mat2;
+    mat1.x = 4;
+    mat1.y = 2;
+    
+    mat2.x = 4;
+    mat2.y = 2;
 
-    Matrix16 b;
 
-    for (int i = 0; i < 4; i++)
-        mat.values[i]=i;
+    for (int i = 0; i < 8; i++)
+    {
+        mat1.values[i]=i;
+        mat2.values[i]=i-0.2;
+    }
 
-    printMatrix16(mat);
-    printMatrix16(b);
-    _SaveMatrix16("test.mat",mat);
-    _LoadMatrix16("test.mat",&b);
 
-*    printMatrix16(mat);
-    printMatrix16(b);
+    printMatrix16(mat1);
+    printMatrix16(mat2);
 
+    Matrix16 a = addMatrix16(mat1,mat2);
+    printMatrix16(a);
+    a = addMatrix16(a,mat1);
+    printMatrix16(a);
+    sigmoidify16(&a);
+    printMatrix16(a);
     
       int val1 = 0;
       int val2 = 0;
@@ -32,7 +42,7 @@ int main(int argc, char** argv){
         val1 = strtol(argv[1],NULL,10);
       if (argc > 2)
         val2 = strtol(argv[2],NULL,10);
-      printf("%s",eval(val1,val2,0)?"true":"false");
-     */
+      printf("%s",eval(val1,val2,0)?"true":"false");*/
+     
     return 0;
 }
--- Code/sourceCode/XoR.c
+++ Code/sourceCode/XoR.c
@@ -25,34 +25,20 @@ int eval(int a, int b , int training){
         _LoadMatrix16("secondBias.mat",&secondBias);
     }
 
-    printMatrix16(secondWeights);
 
     toEval.x=2;
     toEval.y=1;
     toEval.values[0]=(a==0?0:1);
     toEval.values[1]=(b==0?0:1);
     intermediate = multMatrix16(firstWeights,toEval);
-    intermediate = (intermediate,firstBias);
+    intermediate = addMatrix16(intermediate,firstBias);
     sigmoidify16(&intermediate);
 
     lastLayer = multMatrix16(secondWeights,intermediate);
     lastLayer = addMatrix16(lastLayer,secondBias);
     sigmoidify16(&lastLayer);
 
-   /* printf("toEval");
-    printMatrix16(toEval);
-    printf("1s weight");
-    printMatrix16(firstWeights);
-    printf("1s bias");
-    printMatrix16(firstBias);
-    printf("intermediate");
-    printMatrix16(intermediate);
-    printf("2nd weight");
-    printMatrix16(secondWeights);
-    printf("secondBias");
-    printMatrix16(secondBias);
-    printf("lastLayer");
-    printMatrix16(lastLayer);*/
+  
 
     return lastLayer.values[0]>lastLayer.values[1];
     
@@ -66,6 +52,14 @@ void train(int n)
     _LoadMatrix16("firstBias.mat",&firstBias);
     _LoadMatrix16("secondWeights.mat",&secondWeights);
     _LoadMatrix16("secondBias.mat",&secondBias);
+    
+    
+    Matrix16 ErrorMatrix;
+    ErrorMatrix.x = 2;
+    ErrorMatrix.y = 1;
+    ErrorMatrix.values[0] = 0;
+    ErrorMatrix.values[1] = 0;
+
 
     for (int i =0 ; i <n ; i++)
     {
@@ -73,9 +67,21 @@ void train(int n)
          int b = rand()%2;
 
          printf("%i %i -> %i\n",a,b,eval(a,b,1));
-    }
+         Matrix16 localError;
+         localError.x = 2;
+         localError.y = 1;
 
-    //backpropagate
+         localError.values[0] =toEval.values[0] a != b;
+         localError.values[1] =a == b;
+         ErrorMatrix = addMatrix16(ErrorMatrix,localError);
+
+    }
+    
+    //multMatrixValue
+    ErrorMatrix.values[0] = ErrorMatrix.values[0]/(long)n;
+    ErrorMatrix.values[1] = ErrorMatrix.values[1]/(long)n;
+
+    backPropagate();
 
 
     _SaveMatrix16("firstWeights.mat",firstWeights);
@@ -86,3 +92,42 @@ void train(int n)
 
 }
 
+
+
+float BackP(float Error,float ActualOut,float PreviousOut)
+{
+	float lambda = (-Error) * ActualOut * (1 - ActualOut);
+	return lambda * PreviousOut * 0.5; 
+}
+
+void backPropagate( )
+{
+	float TotalError = 0;
+
+    Matrix16 ErrorMatrix;
+
+	for(int x = 0; x < 2;x++)
+	{
+		TotalError += ErrorMatrix.values[GetCoordinates16(x,0,ErrorMatrix)];
+          
+	}
+
+	for(int i = 0; i < 4;i++)
+	{
+		firstBias.values[GetCoordinates16(i,0,firstBias)] += BackP(TotalError , intermediate.values[GetCoordinates16(i,0,intermediate)] , toEval.values[GetCoordinates16(i,0,toEval)]);
+		for(int j = 0; j < 2; j++)
+		{
+			firstWeights.values[GetCoordinates16(i,j,firstWeights)] += BackP(TotalError , intermediate.values[GetCoordinates16(i,0,intermediate)] ,toEval.values[GetCoordinates16(i,0,toEval)]);
+		}
+	}
+
+
+	for(int i = 0; i < 2;i++)
+	{
+		secondBias.values[GetCoordinates16(i,0,secondBias)] += BackP(ErrorMatrix.values[GetCoordinates16(i,0,ErrorMatrix)] , lastLayer.values[GetCoordinates16(i,0,lastLayer)] , intermediate.values[GetCoordinates16(i,0,intermediate)]); 
+		for(int j = 0; j < 4; j++)
+		{
+			secondWeights.values[GetCoordinates16(i,j,secondWeights)] += BackP(ErrorMatrix.values[GetCoordinates16(i,0,ErrorMatrix)] , lastLayer.values[GetCoordinates16(i,0,lastLayer)]  , intermediate.values[GetCoordinates16(i,0,intermediate)] );
+		}
+	}
+}
