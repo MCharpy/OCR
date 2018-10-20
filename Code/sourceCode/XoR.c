1fb4fc8fe7d16030daddef8d8761b6eff6ff7d90
Tacitus701 <36673866+Tacitus701@users.noreply.github.com>
2018-10-20 13:12:25 +0200

    changement nom de fonction
    Signed-off-by: Tacitus701 <36673866+Tacitus701@users.noreply.github.com>

--- Code/sourceCode/XoR.c
+++ Code/sourceCode/XoR.c
@@ -15,7 +15,7 @@ Matrix16 intermediate;
 Matrix16 lastLayer;
 
 void backPropagate(Matrix16 ErrorMatrix, float TotalError);
-float BackP(float Error,float ActualOut,float PreviousOut,Matrix16 * DeltaMatrix, int j , float weight);
+float CreationDeltaMatrix(float Error,float ActualOut,float PreviousOut,Matrix16 * DeltaMatrix, int j , float weight);
 
 
 int eval(int a, int b , int training){
@@ -70,25 +70,17 @@ void train(int n)
          int a = 1;
          int b = 0;
 
-         //printf("%i %i -> %i\n",a,b,eval(a,b,1));
-		 //for(int i = 0; i < n; i++)
-		 //{
 		 printf("%i %i -> %i\n",a,b,eval(a,b,1));
-
 
 		 float error1 = (a != b) - lastLayer.values[0];
 		 float error2 = (a == b) - lastLayer.values[1];
-	
-         //}
+		 float TotalError = (error1*error1)/2 + (error2*error2)/2;
+		 
 		 
 	 	 ErrorMatrix.values[getCoordinates16(0,0,ErrorMatrix)] = (a != b) - lastLayer.values[0];
          ErrorMatrix.values[getCoordinates16(1,0,ErrorMatrix)] = (a == b) - lastLayer.values[1];
-         float TotalError = (error1*error1)/2 + (error2*error2)/2;
-		 //printf("%f",TotalError);
-         backPropagate(ErrorMatrix,TotalError);
-	 	 //printMatrix16(intermediate);
-         //printMatrix16(secondWeights);
-		
+		 
+         backPropagate(ErrorMatrix,TotalError);	
     }
 
 		
@@ -104,7 +96,7 @@ void train(int n)
 
 
 
-float BackP(float Error,float ActualOut,float PreviousOut,Matrix16 * DeltaMatrix, int j , float weight)
+float CreationDeltaMatrix(float Error,float ActualOut,float PreviousOut,Matrix16 * DeltaMatrix, int j , float weight)
 {
 	float delta = -Error * ActualOut * (1 - ActualOut);
 	DeltaMatrix->values[getCoordinates16(j,0,*DeltaMatrix)] += delta * weight;
@@ -113,7 +105,7 @@ float BackP(float Error,float ActualOut,float PreviousOut,Matrix16 * DeltaMatrix
 
 
 
-float BackPFirstLayer(float delta, float hidden , float input)
+float DerivativeFormula(float delta, float hidden , float input)
 
 {
 	return delta * hidden * (1- hidden) * input * 0.5;
@@ -140,28 +132,24 @@ void backPropagate(Matrix16 ErrorMatrix, float TotalError)
 	{
 
 
-		secondBias.values[getCoordinates16(i,0,secondBias)] -= BackPFirstLayer(ErrorMatrix.values[getCoordinates16(i,0,ErrorMatrix)] , lastLayer.values[getCoordinates16(i,0,lastLayer)] , 1.0) ;
+		secondBias.values[getCoordinates16(i,0,secondBias)] -= DerivativeFormula(ErrorMatrix.values[getCoordinates16(i,0,ErrorMatrix)] , lastLayer.values[getCoordinates16(i,0,lastLayer)] , 1.0) ;
 
  
 		for(int j = 0; j < secondWeights.y; j++)
 		{
-			secondWeights.values[getCoordinates16(i,j,secondWeights)] -= BackP(ErrorMatrix.values[getCoordinates16(i,0,ErrorMatrix)] , lastLayer.values[getCoordinates16(i,0,lastLayer)], intermediate.values[getCoordinates16(j,0,intermediate)], &DeltaMatrix , j , secondWeights.values[getCoordinates16(i,j,secondWeights)] );
+			secondWeights.values[getCoordinates16(i,j,secondWeights)] -= CreationDeltaMatrix(ErrorMatrix.values[getCoordinates16(i,0,ErrorMatrix)] , lastLayer.values[getCoordinates16(i,0,lastLayer)], intermediate.values[getCoordinates16(j,0,intermediate)], &DeltaMatrix , j , secondWeights.values[getCoordinates16(i,j,secondWeights)] );
 
 		}
 	}
-	//printMatrix16(DeltaMatrix);
-
-
 
 
 	for(int i = 0; i < firstWeights.x;i++)
 	{
-		firstBias.values[getCoordinates16(i,0,firstBias)] -= BackPFirstLayer(DeltaMatrix.values[getCoordinates16(i,0,DeltaMatrix)] , intermediate.values[getCoordinates16(i,0,intermediate)] , 1.0) ;
-		//printf("%f" ,DeltaMatrix.values[getCoordinates16(i,0,DeltaMatrix)]);
+		firstBias.values[getCoordinates16(i,0,firstBias)] -= DerivativeFormula(DeltaMatrix.values[getCoordinates16(i,0,DeltaMatrix)] , intermediate.values[getCoordinates16(i,0,intermediate)] , 1.0) ;
 	
 		for(int j = 0; j < firstWeights.y; j++)
 		{
-			firstWeights.values[getCoordinates16(i,j,firstWeights)] -= BackPFirstLayer( DeltaMatrix.values[getCoordinates16(i,0,DeltaMatrix)] , intermediate.values[getCoordinates16(i,0,intermediate)] , toEval.values[getCoordinates16(j,0,toEval)]) ;
+			firstWeights.values[getCoordinates16(i,j,firstWeights)] -= DerivativeFormula( DeltaMatrix.values[getCoordinates16(i,0,DeltaMatrix)] , intermediate.values[getCoordinates16(i,0,intermediate)] , toEval.values[getCoordinates16(j,0,toEval)]) ;
 
 		}
 	}
